b9e0eee69691c1b7c88d8b3e7cef84f764dbd3ac
abc810221 <abc810221@gmail.com>
2016-02-19 10:27:07 -0800

    Chat refactor to angular

--- Public/music.js
+++ Public/music.js
@@ -1,19 +1,22 @@
-angular.module('musicApp', ['ngRoute'])
+angular.module('musicApp', ['ngRoute','chat'])
 
-.config(function($routeProvider){
+.config(function($routeProvider, $locationProvider){
   $routeProvider
 
     .when('/', {
       templateUrl: '/youtube.html',
       controller: 'YouTubeController'
     })
+    .otherwise({
+      redirectTo:'/'
+    })
 
+    $locationProvider.html5Mode(true);
 })
 
 .controller('YouTubeController', ['$scope', function($scope){
-
   $scope.message = 'This is working!';
-
 }])
+
 
 
