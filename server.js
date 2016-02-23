f1daa2c5a3e873f2c23830fcc1d31d448c67f8a2
Calvin Le <invisiblebeard@gmail.com>
2016-02-23 13:07:55 -0800

    Prepared files for deployment

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ app.use('/', express.static("./public"));
 
 var server = app.listen(port);
 
-var io = require('socket.io').listen(server);
+// var io = require('socket.io').listen(server);
+
+var io = require('socket.io')({
+  transports: ["xhr-polling"],
+  'polling duration': 10
+}).listen(server);
 
 var users = {};
 var queue = [];
