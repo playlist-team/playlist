1146c3133cf0b2c2abc2b532c51aa0d9a4d1e843
Calvin Le <invisiblebeard@gmail.com>
2016-06-20 11:56:51 -0700

    Removed if-else from nextVideo event

--- client/js/video.js
+++ client/js/video.js
@@ -126,15 +126,11 @@ angular.module('app', ['chat', 'search'])
 
   //Recieve next video from server, plays it and emits queue to controller and time to server
   $window.socket.on('nextVideo', function(video) {
-    if (video !== null) {
       context.current = video;
       $window.socket.emit('setDuration', video.duration);
       player.loadVideoById(video.id);
       $rootScope.$emit('changeQueue');
-    } else {
-      $window.socket.emit('setDuration', video.duration);
-      $rootScope.$emit('changeQueue');
-    }
+    } 
   });
 
   $window.socket.on('stopVideo', function() {
